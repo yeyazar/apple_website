ac3fe7b66ccb6e1df4136b18eb4341428d2cd93f
yeyazar <52497034+yeyazar@users.noreply.github.com>
2024-09-10 23:03:34 +0200

    second

---
+++ src/components/Model.jsx
@@ -0,0 +1,39 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ModelView from "./ModelView";
+import { useState } from "react";
+import { yellowImg } from "../utils";
+
+const Model = () => {
+	const [size, setSize] = useState("small");
+	const [model, setModel] = useState({
+		title: "iPhone 25 Pro in Natural Titanium",
+		color: ["#8F8a81", "#FFE7B9", "#6F6C64"],
+		img: yellowImg,
+	});
+
+	useGSAP(() => {
+		gsap.to("#heading", { y: 0, opacity: 1 });
+	}, []);
+
+	return (
+		<section className="common-padding">
+			<div className="screen-max-width">
+				<h1
+					id="heading"
+					className="section-heading"
+				>
+					Take a closer look
+				</h1>
+
+				<div className="flex flex-col items-center mt-5">
+					<div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+						<ModelView />
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Model;
